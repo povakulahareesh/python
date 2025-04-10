fd8a1f22c4fd00297b4f01ebe9e033389a509ed3
povakulahareesh <hareeshpovakula@gmail.com>
2025-04-10 11:43:47 +0530

    txt

---
+++ errorhandling/errorhandling.py
@@ -0,0 +1,104 @@
+# try:
+#     print(10/5)
+# except:
+#     print("hii")
+# finally:
+#     print("gm")
+
+
+# a=int(input(" enter no:"))
+# b=int(input(" enter no:"))
+# print(a/b)#-->10,5
+# print(a/b)#--> 10,0
+# print("hello")
+
+
+# a=int(input(" enter no:"))
+# b=int(input(" enter no:"))
+# # print(a/b)
+
+# try:
+#     print(a/b)
+
+# except:
+#     print(a/1)
+
+
+
+#  syntax:
+
+# try:
+#     pass
+# except ZeroDivisionError as err:
+#     pass
+# except FileNotFoundError as err:
+#     pass
+# except:
+#     pass
+# finally:
+#     print("hhiii")
+
+
+# fp=None
+# try:
+#     a=int(input("Enter First Number:"))
+#     b=int(input("Enter Second Number:"))
+#     print(a/b)
+#     fp=open("abc.txt",'r')
+#     print(fp.read())
+
+# except ZeroDivisionError as err:
+#     print(a/1)
+
+# except ValueError as err:
+#     print("Enter only Digits")
+
+# except FileNotFoundError as err:
+#     fp=open('abc.txt','r')
+#     print(fp.read())
+# except :
+#     print("New Error! check Code")
+
+
+ 
+
+# a=2
+# print(b)
+# try:
+#     print(b)  #risky code 
+# except:
+#     print("error")
+# else:
+#     print("no error")
+# finally:
+#     print("always")    
+#  error occurs or not finally always will be execcuted
+
+
+
+# a=int(input(" enter no:"))
+# b=int(input(" enter no:"))
+# # print(a/b)
+
+# try:
+#     print(a/b)
+
+# except:
+#     print("hello")
+# else:
+#     print("no error")
+# # finally:
+# #     print("always")
+
+
+#  appending
+
+a=["dd","ss","ewee","iiii","iiiipp"]
+print(a[1])
+# print(a[100]) #index error
+a.append("harish")
+print(a)
+a.hhhaa("jjj")
+print(a)#attribute error
+
+
--- json/csv.py
+++ json/csv.py
@@ -1,5 +1,5 @@
 # import csv
-# fp=open('employee.csv','r')
+# fp=open('emp.csv','r')
 # csvreader=csv.reader(fp)
 # print (csvreader)
 
@@ -34,7 +34,53 @@
 
 # ================================
 
-e=["qq","ee","rr","ooo"]
-print(e[2])
-t=e[2:4]
-print(t)
+# e=["qq","ee","rr","ooo"]
+# print(e[2])
+# t=e[2:4]
+# print(t)
+
+# ======================================
+
+# import requests,csv,json 
+# resp=requests.get('https://dummyjson.com/products')
+# product_data=resp.json()
+
+# print(type(product_data))     #  <class,dict>
+# products=product_data['products']
+
+# print(type(products))         #<class,list>
+
+# # ==========================================
+# import requests,csv,json 
+# # extract data from source - rest api/cloud apis/database
+# resp=requests.get('https://dummyjson.com/products')
+# product_data=resp.json()
+# products=product_data['products']
+
+# #load into beauty.csv and beauty.json
+
+# fp1 = open('beauty.csv','w',newline="")
+# csvwriter=csv.writer(fp1)
+# csvwriter.writerow(['PID','Name','Price','Category'])
+
+# for product in products:
+#     csvwriter.writerow([product['id'],product['title'],product['price'],product['category']])
+
+# print('New CSV File created successfully')
+
+# fp1.close()
+
+
+# =========================================
+
+# fp=open("emp.txt","r")
+# data=fp.read()
+# print(data)
+# fp.close()
+
+
+
+
+a=int(input(" enter no:"))
+b=int(input(" enter no:"))
+print(a/b)
